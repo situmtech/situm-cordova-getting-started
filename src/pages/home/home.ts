66ec35ffe5be93586bd348abb6e87030087c8269
Alberto Doval <alberto.doval@cocodin.com>
2017-07-14 11:29:22 +0200

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare var cordova: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  building: any = undefined;
 
+  constructor(public navCtrl: NavController) {
+    
   }
+
+  fetchBuildings() {
+    let buildings: any = cordova.plugins.Situm.fetchBuildings(res => {
+      this.building = res;
+    });
+    console.log(JSON.stringify(buildings));
+  };
 
 }
