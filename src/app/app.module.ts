0506384cde03749ae03c1bc349f50fc00731d6cc
Oscar Fuentes <oscar.fuentes@cocodin.com>
2017-08-09 13:27:40 +0200

    Added map and POIs to getting started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { GoogleMaps } from '@ionic-native/google-maps';
+import { File } from '@ionic-native/file';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,7 +29,9 @@ import { PositioningPage } from '../pages/positioning/positioning';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    GoogleMaps,
+    File
   ]
 })
 export class AppModule { }
--- src/pages/positioning/positioning.ts
+++ src/pages/positioning/positioning.ts
@@ -1,6 +1,8 @@
 import { Component, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
 import { NavController, NavParams, Platform, Events, LoadingController } from 'ionic-angular';
 import { DomSanitizer } from '@angular/platform-browser';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, LatLng, ILatLng, GroundOverlayOptions, GroundOverlay, MarkerOptions, MarkerIcon } from '@ionic-native/google-maps';
+import { File, IWriteOptions } from '@ionic-native/file'
 
 /**
  * Generated class for the PositioningPage page.
@@ -10,7 +12,6 @@ import { DomSanitizer } from '@angular/platform-browser';
  */
 
 declare var cordova: any;
-declare var google: any;
 
 @Component({
   selector: 'page-positioning',
@@ -35,29 +36,31 @@ export class PositioningPage {
     bearing: ''
   }
 
-  image: any;
-
-  @ViewChild('map') mapElement: ElementRef;
+  image: Blob;
   map: any;
+  poiCategories: any[];
 
   constructor(public platform: Platform, public navCtrl: NavController, public navParams: NavParams, public events: Events, public detector: ChangeDetectorRef,
-    public sanitizer: DomSanitizer, public loadingCtrl: LoadingController) {
+    public sanitizer: DomSanitizer, public loadingCtrl: LoadingController, public googleMaps: GoogleMaps, public file: File) {
     this.building = this.navParams.get('building');
   }
 
   ionViewDidEnter() {
     this.platform.ready().then(() => {
-      cordova.plugins.Situm.fetchIndoorPOIsFromBuilding(this.building, (res: any) => {
-        console.log(res);
-      });
-      cordova.plugins.Situm.fetchOutdoorPOIsFromBuilding(this.building, (res: any) => {
-        console.log(res);
-      });
-      cordova.plugins.Situm.fetchEventsFromBuilding(this.building, (res: any) => {
-        console.log(res);
-      });
+      // cordova.plugins.Situm.fetchIndoorPOIsFromBuilding(this.building, (res: any) => {
+      //   console.log(res);
+      //   cordova.plugins.Situm.requestDirections([res[0], res[1]], (res) => {
+      //     console.log(res);
+      //   });
+      // });
+      // cordova.plugins.Situm.fetchOutdoorPOIsFromBuilding(this.building, (res: any) => {
+      //   console.log(res);
+      // });
+      // cordova.plugins.Situm.fetchEventsFromBuilding(this.building, (res: any) => {
+      //   console.log(res);
+      // });
       cordova.plugins.Situm.fetchPoiCategories((res: any) => {
-        console.log(res);
+        this.poiCategories = res;
       });
     });
   }
@@ -88,36 +91,85 @@ export class PositioningPage {
     cordova.plugins.Situm.stopPositioning(() => { });
   }
 
+  private showPois() {
+    cordova.plugins.Situm.fetchIndoorPOIsFromBuilding(this.building, (res: any) => {
+      if (this.poiCategories && this.map) {
+        res.forEach(element => {
+          let category = this.poiCategories.find((poiCategory: any) => {
+            return poiCategory.poiCategoryCode == element.category
+          });
+          element.category = category;
+          let markerPosition: ILatLng = {
+            lat: element.coordinate.latitude,
+            lng: element.coordinate.longitude
+          }
+          let icon: MarkerIcon = {
+            url: element.category.icon_selected,
+            size: {
+              height: 35,
+              width: 35
+            }
+          }
+          let markerOptions: MarkerOptions = {
+            icon: icon,
+            position: markerPosition,
+
+          };
+          this.map.addMarker(markerOptions);
+          console.log(element);
+        });
+      }
+    });
+  }
+
   private showMap() {
     if (!this.map) {
-      let loading = this.loadingCtrl.create({
-        content: "Cargando mapa..."
-      });
-      loading.present();
-      cordova.plugins.Situm.fetchFloorsFromBuilding(this.building, (res) => {
-        let floor = res[0];
-        cordova.plugins.Situm.fetchMapFromFloor(floor, (res) => {
-          this.image = this.sanitizer.bypassSecurityTrustResourceUrl("data:image/png;base64," + res.data);
-          let latlng = new google.maps.LatLng(this.building.center.latitude, this.building.center.longitude);
-
-          let mapOptions = {
-            center: latlng,
-            zoom: 18,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            heading: this.building.rotation
-          };
-
-          let mapEle = this.mapElement.nativeElement;
-
-          this.map = new google.maps.Map(mapEle, mapOptions);
-          google.maps.event.addListenerOnce(this.map, 'idle', () => {
-            loading.dismiss();
+      this.platform.ready().then(() => {
+        let loading = this.loadingCtrl.create({
+          content: "Cargando mapa..."
+        });
+        loading.present();
+        console.log(this.building);
+        cordova.plugins.Situm.fetchFloorsFromBuilding(this.building, (res) => {
+          let floor = res[0];
+          cordova.plugins.Situm.fetchMapFromFloor(floor, (res) => {
+            this.image = this.b64toBlob(res.data, "image/png", 512);
+            let imageOptions: IWriteOptions = {
+              replace: true
+            }
+            let element: HTMLElement = document.getElementById('map');
+            let center: LatLng = new LatLng(this.building.center.latitude, this.building.center.longitude);
+            let options: GoogleMapOptions = {
+              camera: {
+                target: center,
+                zoom: 19
+              }
+            };
+            this.map = this.googleMaps.create(element, options);
+            this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+              // loading.dismiss();
+              let boundsSW: LatLng = new LatLng(this.building.bounds.southWest.latitude, this.building.bounds.southWest.longitude);
+              let boundsNE: LatLng = new LatLng(this.building.bounds.northEast.latitude, this.building.bounds.northEast.longitude);
+              let bounds = [
+                { lat: this.building.bounds.southWest.latitude, lng: this.building.bounds.southWest.longitude },
+                { lat: this.building.bounds.northEast.latitude, lng: this.building.bounds.northEast.longitude }
+              ];
+              this.file.writeFile(this.file.externalApplicationStorageDirectory + "files", "mapa.png", this.image, imageOptions).then(() => {
+                let groundOptions: GroundOverlayOptions = {
+                  bounds: bounds,
+                  url: this.file.externalApplicationStorageDirectory + "files/mapa.png",
+                  bearing: this.building.rotation * 180 / Math.PI
+                }
+                this.map.addGroundOverlay(groundOptions).then(() => {
+                  loading.dismiss();
+                }).catch((err: any) => {
+                  console.log(err);
+                });
+              }).catch((err: any) => {
+                console.log(err);
+              });
+            });
           });
-          let boundsNE = new google.maps.LatLng(this.building.bounds.northEast.latitude, this.building.bounds.northEast.longitude);
-          let boundsSW = new google.maps.LatLng(this.building.bounds.southWest.latitude, this.building.bounds.southWest.longitude);
-          let boundsOverlay = new google.maps.LatLngBounds(boundsSW, boundsNE);
-          let overlay = new google.maps.GroundOverlay(floor.mapUrl, boundsOverlay);
-          overlay.setMap(this.map);
         });
       });
     }
@@ -125,6 +177,31 @@ export class PositioningPage {
 
   ionViewWillLeave() {
     this.stopPositioning();
+  }
+
+  // convert base64 to Blob
+  b64toBlob(b64Data, contentType, sliceSize) {
+    let byteCharacters = atob(b64Data);
+
+    let byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      let slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      let byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      let byteArray = new Uint8Array(byteNumbers);
+
+      byteArrays.push(byteArray);
+
+    }
+
+    let blob = new Blob(byteArrays, { type: contentType });
+
+    return blob;
   }
 
 }
